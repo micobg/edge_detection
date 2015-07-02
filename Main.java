a8385286bb588e1f197d2917bdcdfb7c7d3e75e9
micobg <micobg@gmail.com>
2015-07-02 15:45:18 +0300

    Sobel filter is done

--- Main.java
+++ Main.java
@@ -1,145 +1,17 @@
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int     i, j;
-        FileInputStream inFile = new FileInputStream(args[0]);
-        BufferedImage inImg = ImageIO.read(inFile);
-        int width = inImg.getWidth();
-        int height = inImg.getHeight();
-        int[] output = new int[width*height];
-        int[] pixels = inImg.getRaster().getPixels(0,0,width,height,(int[])null);
+        FileInputStream file = new FileInputStream(args[0]);
 
-        double Gx;
-        double Gy;
-        double G;
+        SobelFilterGrayscale sobelFilterGrayscale = new SobelFilterGrayscale(file);
+        sobelFilterGrayscale.applyFilter();
 
-        for(i = 0 ; i < width ; i++ )
-        {
-            for(j = 0 ; j < height ; j++ )
-            {
-                if (i==0 || i==width-1 || j==0 || j==height-1)
-                    G = 0; // Image boundary cleared
-                else{
-                    Gx = pixels[(i+1)*height + j-1] + 2*pixels[(i+1)*height +j] + pixels[(i+1)*height +j+1] -
-                            pixels[(i-1)*height +j-1] - 2*pixels[(i-1)*height+j] - pixels[(i-1)*height+j+1];
-                    Gy = pixels[(i-1)*height+j+1] + 2*pixels[i*height +j+1] + pixels[(i+1)*height+j+1] -
-                            pixels[(i-1)*height+j-1] - 2*pixels[i*height+j-1] - pixels[(i+1)*height+j-1];
-                    G  = Math.hypot(Gx, Gy);
-                }
-
-                output[i*height+j] = (int)G;
-            }
+        if (args.length > 1) {
+            sobelFilterGrayscale.save(args[1]);
         }
 
-
-        BufferedImage outImg = new BufferedImage(width,height,BufferedImage.TYPE_BYTE_GRAY);
-        outImg.getRaster().setPixels(0,0,width,height,output);
-        FileOutputStream outFile = new FileOutputStream("result.jpg");
-        ImageIO.write(outImg,"JPG",outFile);
-
-        JFrame TheFrame = new JFrame("Result");
-
-        JLabel TheLabel = new JLabel(new ImageIcon(outImg));
-        TheFrame.getContentPane().add(TheLabel);
-
-
-        TheFrame.setSize(width, height);
-
-        TheFrame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-        TheFrame.setVisible(true);
-
-
-
-
-        return;
-
-
-//
-//        File file = new File(args[0]);
-//        try {
-//            BufferedImage image = ImageIO.read(file);
-////            int[][] result = imageToIntArray(image);
-//            imageToByteArray(file);
-//        } catch (IOException e) {
-//            System.err.println("Cannot read the file.");
-//        }
-    }
-
-    private static int[][] imageToIntArray(BufferedImage image) {
-
-        final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-        final int width = image.getWidth();
-        final int height = image.getHeight();
-        final boolean hasAlphaChannel = image.getAlphaRaster() != null;
-
-        int[][] result = new int[height][width];
-        if (hasAlphaChannel) {
-            final int pixelLength = 4;
-            for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
-                int argb = 0;
-                argb += (((int) pixels[pixel] & 0xff) << 24); // alpha
-                argb += ((int) pixels[pixel + 1] & 0xff); // blue
-                argb += (((int) pixels[pixel + 2] & 0xff) << 8); // green
-                argb += (((int) pixels[pixel + 3] & 0xff) << 16); // red
-                result[row][col] = argb;
-                col++;
-                if (col == width) {
-                    col = 0;
-                    row++;
-                }
-            }
-        } else {
-            final int pixelLength = 3;
-            for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
-                int argb = 0;
-                argb += -16777216; // 255 alpha
-                argb += ((int) pixels[pixel] & 0xff); // blue
-                argb += (((int) pixels[pixel + 1] & 0xff) << 8); // green
-                argb += (((int) pixels[pixel + 2] & 0xff) << 16); // red
-                result[row][col] = argb;
-
-                if (argb != -1) {
-                    System.out.println(argb);
-                }
-                col++;
-                if (col == width) {
-                    col = 0;
-                    row++;
-                }
-            }
-        }
-
-        return result;
-    }
-
-    private static byte[] imageToByteArray(File image) throws FileNotFoundException {
-        FileInputStream fis = new FileInputStream(image);
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        byte[] buf = new byte[1024];
-        try {
-            for (int readNum; (readNum = fis.read(buf)) != -1; ) {
-                //Writes to this byte array output stream
-                bos.write(buf, 0, readNum);
-                System.out.println("read " + readNum + " bytes,");
-            }
-        } catch (IOException ex) {
-            System.err.println("Cannot read the file.");
-        }
-
-        byte[] bytes = bos.toByteArray();
-
-        return bytes;
+        sobelFilterGrayscale.show();
     }
 }
---
+++ SobelFilterGrayscale.java
@@ -0,0 +1,138 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class SobelFilterGrayscale {
+
+    /**
+     * Parameters of the incoming image.
+     */
+    private int width;
+    private int height;
+    private int[] pixels;
+
+    /**
+     * Keep last iterated column (left) and upper cell to be able to write result in the same array.
+     */
+    private int upperCell = 0;
+    private int[][] leftColumn;
+
+    /**
+     * We should keep left column for calculations and build new left column (for next iteration) at the same time.
+     */
+    private int leftColumnRead = 0;
+    private int leftColumnWrite = 1;
+
+    private BufferedImage outputImage = null;
+
+    /**
+     * Open given image.
+     *
+     * @param file path to file
+     * @throws IOException on reading file exception
+     */
+    SobelFilterGrayscale(FileInputStream file) throws IOException {
+        BufferedImage inImg = ImageIO.read(file);
+
+        width = inImg.getWidth();
+        height = inImg.getHeight();
+        pixels = inImg.getRaster().getPixels(0, 0, width, height, (int[])null);
+
+        leftColumn = new int[2][height];
+    }
+
+    /**
+     * Apply Sobel filter to the image.
+     */
+    public void applyFilter() {
+        int i, j, G;
+        for(i = 0 ; i < width ; i++ ) {
+            for(j = 0 ; j < height ; j++ ) {
+                if (i == 0 || i == width - 1 || j == 0 || j == height - 1) {
+                    // border case
+                    G = 0;
+                } else {
+                    G = sobelOperator(i, j);
+                }
+
+                // keep left column and upper cell
+                upperCell = pixels[j * width + i];
+                leftColumn[leftColumnWrite][j] = pixels[j * width + i];
+
+                pixels[j * width + i] = G;
+            }
+
+            // switch reader and writer
+            leftColumnRead = (leftColumnRead + 1) % 2;
+            leftColumnWrite = (leftColumnWrite + 1) % 2;
+        }
+    }
+
+    /**
+     * Save the image to given path.
+     *
+     * @param outputFileName path t save
+     * @throws IOException if can not save the file to given path
+     */
+    public void save(String outputFileName) throws IOException {
+        buildOutputImage();
+
+        FileOutputStream outFile = new FileOutputStream(outputFileName);
+        ImageIO.write(outputImage, "JPG", outFile);
+    }
+
+    /**
+     * Show the image.
+     */
+    public void show() {
+        JFrame TheFrame = new JFrame("Result");
+
+        if (outputImage == null) {
+            buildOutputImage();
+        }
+
+        JLabel TheLabel = new JLabel(new ImageIcon(outputImage));
+        TheFrame.getContentPane().add(TheLabel);
+
+        TheFrame.setSize(width, height);
+        TheFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        TheFrame.setVisible(true);
+    }
+
+    /**
+     * Apply Sobel operator for one pixel of picture.
+     *
+     * @param i width coordinates of the pixel
+     * @param j height coordinates of the pixel
+     *
+     * @return new value of the pixel
+     */
+    private int sobelOperator(int i, int j) {
+        double Gx =
+            pixels[(j - 1) * width + i + 1] + 2 * pixels[j * width + i + 1] + pixels[(j + 1) * width + i + 1] -
+            leftColumn[leftColumnRead][j - 1] - 2 * leftColumn[leftColumnRead][j] - leftColumn[leftColumnRead][j + 1];
+        double Gy =
+            leftColumn[leftColumnRead][j + 1] + 2 * pixels[(j + 1) * width + i] + pixels[(j + 1) * width + i + 1] -
+            leftColumn[leftColumnRead][j-1] - 2 * upperCell - pixels[(j - 1) * width + i + 1];
+
+        return (int)Math.hypot(Gx, Gy);
+    }
+
+    /**
+     * Build output image to save or show it.
+     */
+    private void buildOutputImage() {
+        outputImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        outputImage.getRaster().setPixels(0, 0, width, height, pixels);
+    }
+
+}
